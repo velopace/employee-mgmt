b5b4805dcb5356f32cc0fc53f30e29e19bcd6ecb
velopace <velopace@outlook.com>
2023-06-09 10:18:03 -0400

    db config

---
+++ Data/EmployeeMgmtDbContext.cs
@@ -0,0 +1,15 @@
+﻿using employee_mgmt.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace employee_mgmt.Data
+{
+	public class EmployeeMgmtDbContext : DbContext
+	{
+        public EmployeeMgmtDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using employee_mgmt.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<EmployeeMgmtDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeMgmtConnectionString")));
 
 var app = builder.Build();
 
