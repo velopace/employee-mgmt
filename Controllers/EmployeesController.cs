52f7fb80ff7a28ae6ca07aee4b046815777f39c2
velopace <velopace@outlook.com>
2023-06-12 10:58:59 -0400

    post employee

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using employee_mgmt.Data;
 using Microsoft.EntityFrameworkCore;
+using employee_mgmt.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -27,6 +28,17 @@ namespace employee_mgmt.Controllers
             var employees = await _employeeMgmtDbContext.Employees.ToListAsync();
 
             return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+
+            await _employeeMgmtDbContext.Employees.AddAsync(employeeRequest);
+            await _employeeMgmtDbContext.SaveChangesAsync();
+
+            return Ok(employeeRequest);
         }
     }
 }
